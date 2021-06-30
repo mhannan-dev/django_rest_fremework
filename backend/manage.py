a3ca53a8384b5869b4ba1f12ffef6fbb24e8986b
Muhammad Hannan <mdhannan.info@gmail.com>
2021-07-01 02:07:12 +0600

    reformat

--- backend/manage.py
+++ backend/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drf_ecomm.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'root_site.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- backend/root_site/urls.py
+++ backend/root_site/urls.py
@@ -1,4 +1,4 @@
-"""drf_ecomm URL Configuration
+"""root_site URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
